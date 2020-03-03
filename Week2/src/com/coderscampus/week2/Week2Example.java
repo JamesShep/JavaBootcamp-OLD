e41e71886874a4b62cf9db3178ef3208e38e07d4
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-03-03 23:19:32 +1000

    Week 1.13 - AND and OR operators

--- Week2/src/com/coderscampus/week2/Week2Example.java
+++ Week2/src/com/coderscampus/week2/Week2Example.java
@@ -7,10 +7,18 @@ public class Week2Example {
     public static void main (String [] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter an input: ");
-        String userInput = scanner.nextLine();
+        System.out.println("What is your name: ");
+        String name = scanner.nextLine();
+        System.out.println("What is your age: ");
+        String age = scanner.nextLine();
 
-        System.out.println("Your typed in: " + userInput);
+        if ((name.equals("James") || name.equals("JD")) && age.equals("26")) {
+            System.out.println("Holy cow. It's me!");
+        } else {
+            System.out.println("Hello: " + name);
+        }
+
+        scanner.close();
     }
 
 }
