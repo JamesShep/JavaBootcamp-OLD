4fe58d86be15f6b6298159aea85d84c888392c2e
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-03-03 22:43:35 +1000

    Week 1.12 - Reading User Input

--- Week2/src/com/coderscampus/week2/Week2Example.java
+++ Week2/src/com/coderscampus/week2/Week2Example.java
@@ -1,37 +1,16 @@
 package com.coderscampus.week2;
 
+import java.util.Scanner;
+
 public class Week2Example {
 
     public static void main (String [] args) {
 
-        int age = 18;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an input: ");
+        String userInput = scanner.nextLine();
 
-        if(age >= 18) {
-            System.out.println("You are an adult");
-            if (age > 65) {
-                System.out.println("You are a senior");
-            }
-        }
-        else if (age >= 13) {
-            System.out.println("You are a teenager");
-        }
-        else if (age == 12) {
-            System.out.println("You are a pre-teen");
-        }
-        else {
-            System.out.println("You are a child");
-
-        }
-        String output = "Your age is " + age;
-
-        System.out.println(output);
-
-        if (output.equals("Your age is 18")) {
-            System.out.println("Yay you can vote!");
-        }
-        else if (output.equals("Your age is 21")) {
-            System.out.println("Yay you can drink!");
-        }
+        System.out.println("Your typed in: " + userInput);
     }
 
 }
