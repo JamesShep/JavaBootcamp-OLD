c74a4c1ea0ceb2d3e76f85b608171ff34d8b4779
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-03-04 19:31:23 +1000

    Week 2.6 - For Loops

---
+++ Week2/src/com/coderscampus/objects/Loops.java
@@ -0,0 +1,17 @@
+package com.coderscampus.objects;
+
+public class Loops {
+
+    public static void main (String [] args) {
+
+        //initialisation, termination condition, increment
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i + ": ");
+            System.out.println(i);
+        }
+
+        System.out.println("Done!");
+    }
+
+}
