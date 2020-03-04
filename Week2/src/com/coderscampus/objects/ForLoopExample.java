1191a1eb4dc1ad5e729a5f8387bc248996dbee37
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-03-04 21:49:27 +1000

    Week 2.8 - Increment Shortcuts

--- Week2/src/com/coderscampus/objects/ForLoopExample.java
+++ Week2/src/com/coderscampus/objects/ForLoopExample.java
@@ -11,4 +11,20 @@ public class ForLoopExample {
 
         System.out.println("Done!");
     }
+
+    void incrementExample1 () {
+        for (int j=0; j<10; j++) {
+            System.out.println(j);
+        }
+        for (int j=9; j>=0; j--) {
+            System.out.println(j);
+        }
+        System.out.println("Done!");
+    }
+
+    void incrementExample2 (int j) {
+        System.out.println("Inside incrementExample2 method j = " + j);
+
+
+    }
 }
--- Week2/src/com/coderscampus/objects/Loops.java
+++ Week2/src/com/coderscampus/objects/Loops.java
@@ -10,8 +10,16 @@ public class Loops {
         /*WhileLoopExample whileExample1 = new WhileLoopExample();
         whileExample1.whileLoopExample1();*/
 
-        WhileLoopExample whileExample2 = new WhileLoopExample();
-        whileExample2.whileLoopExample2();
+        /*WhileLoopExample whileExample2 = new WhileLoopExample();
+        whileExample2.whileLoopExample2();*/
+
+        /*ForLoopExample forExample = new ForLoopExample();
+        forExample.incrementExample1();*/
+
+        ForLoopExample forExample = new ForLoopExample();
+        int j = 0;
+        forExample.incrementExample2(j++);
+        System.out.println("outside method j = " + j);
 
     }
 }
