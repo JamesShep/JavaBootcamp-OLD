f22b67f877822529a5f6b20f9537664af75ef3ad
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-03-11 18:22:37 +1000

    Week 3.6 - Arrays Part I

--- Week3/src/com/coderscampus/week5/UserApplication.java
+++ Week3/src/com/coderscampus/week5/UserApplication.java
@@ -5,8 +5,22 @@ public class UserApplication {
     public static void main(String[] args) {
         UserService userService = new UserService();
 
+        // Arrays allow us to store multiple objects (or primitives) into
+        // ONE variable - Note: an Array is a one type of Data Structure
 
-        User jamesUser = userService.createUser("test@email.com", "password123");
-        System.out.println(jamesUser);
+        // [0], [1], [2], [3], [4]
+        User[] users = new User[5];
+
+        users[0] = userService.createUser("user1","password1");
+        users[1] = userService.createUser("user2","password2");
+        users[2] = userService.createUser("user3","password3");
+        users[3] = userService.createUser("user4","password4");
+        users[4] = userService.createUser("user5","password5");
+
+        User user1 = userService.createUser("user1", "password1");
+        User user2 = userService.createUser("user2", "password2");
+        User user3 = userService.createUser("user3", "password3");
+        User user4 = userService.createUser("user4", "password4");
+        User user5 = userService.createUser("user5", "password5");
     }
 }
