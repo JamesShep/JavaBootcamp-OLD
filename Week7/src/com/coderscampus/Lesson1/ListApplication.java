e2eb24a291464dd5614af97e682048bb68369ff4
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-05-04 13:53:28 +1000

    Week 7.2 - ArrayList

--- Week7/src/com/coderscampus/Lesson1/ListApplication.java
+++ Week7/src/com/coderscampus/Lesson1/ListApplication.java
@@ -15,16 +15,42 @@ public class ListApplication {
         names[3] = "Trudy Hamilton";
         //names[4] = "Jeff Bezos";
 
+        System.out.println("Arrays Example: ");
         for (String name : names) {
             System.out.println(name);
         }
 
+        System.out.println("------------------");
+        System.out.println("");
         // Collections -> Java solution to common data structure problems
         // List (a type of collection)
         // Set (a type of collection)
         // Map (a type of collection)
 
         List<String> elements = new ArrayList<String>();
+        elements.add("James Sheppard");
+        elements.add("Trevor Page");
+        elements.add("Elon Musk");
+        elements.add("Trudy Hamilton");
+        elements.add("Jeff Bezos");
+        elements.add("Alex Stewart");
+
+        System.out.println("List Example: ");
+        printArrayListToConsole(elements);
+
+        System.out.println("");
+
+        elements.remove(1);
+
+        elements.remove("Trudy Hamilton");
+        printArrayListToConsole(elements);
+
+    }
+
+    private static void printArrayListToConsole(List<String> elements) {
+        for (String element : elements) {
+            System.out.println(element);
+        }
     }
 
 }
