39ce987aee93265cbd79f699a522d89a689ff84f
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-05-04 19:10:09 +1000

    Week 7.10 - Stream Map

--- Week7/src/com/coderscampus/Lesson1/StreamApplication.java
+++ Week7/src/com/coderscampus/Lesson1/StreamApplication.java
@@ -2,13 +2,48 @@ package com.coderscampus.Lesson1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StreamApplication {
 
     public static void main(String[] args) {
-        
+
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new Car("Tesla", "Model S", 2019));
+        cars.add(new Car("Tesla", "Model S", 2018));
+        cars.add(new Car("Tesla", "Model X", 2016));
+        cars.add(new Car("Tesla", "Model 3", 2019));
+        cars.add(new Car("Tesla", "Model S", 2019));
+        cars.add(new Car("Ford", "F150", 2017));
+        cars.add(new Car("Toyota", "Corolla", 1997));
+        cars.add(new Car("Toyota", "Celica", 2002));
+
+        List<String> models = cars.stream()
+                                  .map(car -> car.getModel())
+                                  .collect(Collectors.toList());
+
+        Set<String> brands = cars.stream()
+                                 .map(car -> car.getBrand())
+                                 .collect(Collectors.toSet());
+
+        models.stream()
+              .forEach(model -> System.out.println(model));
+
+        System.out.println("-----------");
+
+        brands.stream()
+              .forEach(brand -> System.out.println(brand));
+
+
+
+        //example1();
+    }
+
+    private static void example1() {
         List<String> names = new ArrayList<>();
-        
+
         populateNames(names);
 
         // standard for loop
@@ -42,7 +77,7 @@ public class StreamApplication {
         names.add("George Mckeown");
         names.add("Misha Ali");
         names.add("Tanya James");
-        names.add("Taybah Wilkinson");
+        names.add("Talyah Wilkinson");
         names.add("Zac Holman");
         names.add("Kacie Dunlap");
 
