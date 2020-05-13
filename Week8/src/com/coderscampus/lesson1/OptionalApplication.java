c6290f64765edc564ebd69c1c79f1cd3ea734028
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-05-13 22:28:53 +1000

    Week 8.2 - Setting Optional Values

--- Week8/src/com/coderscampus/lesson1/OptionalApplication.java
+++ Week8/src/com/coderscampus/lesson1/OptionalApplication.java
@@ -6,6 +6,7 @@ public class OptionalApplication {
 
     public static void main(String[] args) {
 
+        // Lesson 1
         Optional<String> fullName = Optional.of("James Sheppard");
 
         System.out.println(fullName);
@@ -13,6 +14,14 @@ public class OptionalApplication {
         String james = fullName.get();
 
         System.out.println(james);
+
+        // Lesson 2
+        Recipe cookies = new Recipe();
+        cookies.setName("Chocolate Chip Cookies");
+        cookies.setInstructions("\n1: Pre-heat oven\n2: Put dough on baking sheet\n" +
+                "3: Cook dough for 15 minutes");
+
+        System.out.println(cookies);
     }
 
 }
