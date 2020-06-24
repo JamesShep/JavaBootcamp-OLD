e95591aa4b9c880b5a3f940b448a012739c3eed9
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-06-24 17:35:13 +1000

    Week 9.3 - More Unit Test Examples

--- Week9/src/test/CustomArrayListTest.java
+++ Week9/src/test/CustomArrayListTest.java
@@ -15,19 +15,19 @@ class CustomArrayListTest {
     // -- Repeat
 
     @Test
-    void should_add_one_item_to_list() {
+    void should_add_one_item_to_list () {
         // Three A's
         // Arrange
         // Act
         // Assert
 
         // ARRANGE
-        CustomList<Integer> customList = new CustomArrayList<>();
+        CustomList<Integer> sut = new CustomArrayList<>();
 
         // ACT
-        customList.add(10);
-        Integer expectedResult = customList.get(0);
-        Integer expectedSize = customList.getSize();
+        sut.add(10);
+        Integer expectedResult = sut.get(0);
+        Integer expectedSize = sut.getSize();
 
         // ASSERT
         // What would we expect to happen?
@@ -36,5 +36,53 @@ class CustomArrayListTest {
 
         assertEquals(10, expectedResult);
         assertEquals(1, expectedSize);
+    }
+
+    @Test
+    void should_add_11_items_to_list () {
+        // Three A's
+        // Arrange
+        // Act
+        // Assert
+
+        // ARRANGE
+        // sut == System under test
+        CustomList<Integer> sut = new CustomArrayList<>();
+
+        // ACT
+        for (int i = 1; i <= 11; i++ ) {
+            sut.add(i);
+        }
+
+        // ASSERT
+
+        for (int i = 0; i < 11; i++) {
+            assertEquals(i + 1, sut.get(i));
+        }
+        assertEquals(11, sut.getSize());
+    }
+
+    @Test
+    void should_add_21_items_to_list () {
+        // Three A's
+        // Arrange
+        // Act
+        // Assert
+
+        // ARRANGE
+        // sut == System under test
+        CustomList<Integer> sut = new CustomArrayList<>();
+
+        // ACT
+        for (int i = 1; i <= 21; i++ ) {
+            sut.add(i);
+        }
+
+        // ASSERT
+
+        for (int i = 0; i < 21; i++) {
+            assertEquals(i + 1, sut.get(i));
+        }
+        assertEquals(21, sut.getSize());
     }
 }
