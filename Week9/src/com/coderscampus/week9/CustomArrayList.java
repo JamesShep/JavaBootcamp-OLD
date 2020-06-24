ed2dde7adf0b55eb09a687b176bea661d929465f
JamesShep <26717741+JamesShep@users.noreply.github.com>
2020-06-24 13:56:35 +1000

    Week 9.1 - What is Unit Testing

---
+++ Week9/src/com/coderscampus/week9/CustomArrayList.java
@@ -0,0 +1,20 @@
+package com.coderscampus.week9;
+
+public class CustomArrayList<T> implements CustomList<T>{
+    Object[] items = new Object[10];
+
+    @Override
+    public boolean add(T item) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+}
---
+++ Week9/src/test/CustomArrayListTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomArrayListTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+}
